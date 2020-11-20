e64070adf96ed8272a82e45e49543d6e0b0397d3
Mert Ülkü <mert-ulku@hotmail.com>
2020-11-20 16:12:29 +0300

    store structure add

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,70 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import shopService from '../services/shopService'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+
+    listings: [],
+    orders: []
+
+  },
+  getters: {
+
+    listings(state){
+      return state.listings
+    },
+    orders(state){
+      return state.orders
+    }
+
+  },
+  mutations: {
+
+    addToBasket(state, payload){
+      state.orders = [...state.orders, payload]
+    },
+    setListings(state, payload){
+      state.listings = [...state.listings, payload]
+    }
+  },
+  actions: {
+
+    async getListings({commit}){
+
+      try {
+
+        const response = await shopService.getListings()
+
+        commit('setListings', response.data)
+
+        return response
+
+      } catch(error) {
+
+        console.log(error)
+
+      }
+
+    },
+    addToBasket({commit}, payload){
+      commit('addToBasket', payload)
+    },
+    async submitOrder(_ ,{ orders }){
+      
+      try {
+
+        const response = await shopService.submitOrder(orders)
+
+        return response
+
+      } catch(error) {
+        console.log(error)
+      }
+    }
+
+  }
+
 });
